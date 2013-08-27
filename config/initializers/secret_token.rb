24bba2fb2187ee1ae7d4de3d198c9743af241c47
Xander Miller <xander.miller@gmail.com>
2013-08-27 17:26:58 -0400

    First Commit, let the fun begin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectNightMeta::Application.config.secret_key_base = '4703d24413f980d4fcea7dd45630469b17e88d8b0966c1e049b68e0bf2307de04b350f951d2360a8421c1e2e5ec5a509fa9596a0374791c8a83a1dc74082f65f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectNightMeta::Application.config.session_store :cookie_store, key: '_ProjectNightMeta_session'
