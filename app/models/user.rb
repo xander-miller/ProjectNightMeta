76fe14156c6228c91c5b29886391e065eaa48a93
Long On <on.long.on@goomail.fake>
2013-09-14 13:53:01 -0400

    Add unit tests and method to register user given authentication hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,45 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  #devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :trackable, :validatable
+  #
+  #devise :omniauthable # disabled for now
+
+  validates_uniqueness_of :mu_id
+
+
+  # class methods
+
+  def self.register_with(omniauth_hash)
+    h = omniauth_hash
+    user = new()
+    user.provider = h["provider"]
+    user.uid = h["uid"]
+
+    raw_info = h["extra"]["raw_info"]
+    user.mu_id = raw_info["id"]
+    user.mu_name = raw_info["name"]
+    user.mu_link = raw_info["link"]
+    user.city = raw_info["city"]
+    user.country = raw_info["country"]
+
+    photo = raw_info["photo"]
+    user.mu_photo_link = photo["photo_link"]
+    user.mu_highres_link = photo["highres_link"]
+    user.mu_thumb_link = photo["thumb_link"]
+    user.mu_photo_id = photo["photo_id"]
+
+    creds = h["credentials"]
+    user.authentication_token = creds["token"]
+    user.mu_refresh_token = creds["refresh_token"]
+    user.mu_expires_at = creds["expires_at"]
+    user.mu_expires = creds["expires"]
+
+    user
+  end
+
+
+  # instance methods
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,46 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def new_user_hash
+    # source: https://github.com/tapster/omniauth-meetup
+    {
+      "provider"=>"meetup",
+      "uid"=>12345,
+      "info"=> {
+        "id"=>111,
+        "name"=>"elvis",
+        "photo_url"=>"http://photos3.meetupstatic.com/photos/member_pic_111.jpeg"
+        },
+      "credentials"=> {
+          "token"=>"abc123...",         # OAuth 2.0 access_token, which you may wish to store
+          "refresh_token"=>"bcd234...", # This token can be used to refresh your access_token later
+          "expires_at"=>1324720198,     # when the access token expires (Meetup tokens expire in 1 hour)
+          "expires"=>true
+        },
+      "extra"=> {
+        "raw_info"=> {
+          "lon"=>-90.027181,
+          "link"=>"http://www.meetup.com/members/111",
+          "lang"=>"en_US",
+          "photo"=> {
+            "photo_link"=> "http://photos3.meetupstatic.com/photos/member_pic_111.jpeg",
+            "highres_link"=> "http://photos1.meetupstatic.com/photos/member_pic_111_hires.jpeg",
+            "thumb_link"=> "http://photos1.meetupstatic.com/photos/member_pic_111_thumb.jpeg",
+            "photo_id"=>111
+          },
+          "city"=>"Memphis",
+          "country"=>"us",
+          "visited"=>1325001005000,
+          "id"=>111,
+          "topics"=>[],
+          "joined"=>1147652858000,
+          "name"=>"elvis",
+          "other_services"=> {"twitter"=>{"identifier"=>"@elvis"}},
+          "lat"=>35.046677
+        }
+      }
+    }
+  end
+
 end
