20febd34c26a9dbe6931ec91416feffcd93cd446
Long On <on.long.on@goomail.fake>
2013-10-03 00:26:04 -0400

    Add check authorization before_filter and current_user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
+require 'ruby_meetup'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  protected
+
+    # before_filter for accessing user pages
+    def check_authorized
+      if session[:mu_id]
+        current_user
+        return true
+      end
+      redirect_to "/login"
+    end
+
+    def current_user
+      @user = User.find_by_mu_id(session[:mu_id]) unless @user
+      @user
+    end
+
 end
