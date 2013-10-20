8e0ce627552b07347f34659e1e6532d6a84f843a
Long On <on.long.on@goomail.fake>
2013-10-20 18:22:21 -0400

    Markup Project detail page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,7 @@ module ApplicationHelper
     @location = Location.new_session(session) unless @location
     @location
   end
+  def get_user_name(user, project_maintainers)
+    user.in_maintainers(project_maintainers) ? "*" + user.mu_name : user.mu_name
+  end
 end
