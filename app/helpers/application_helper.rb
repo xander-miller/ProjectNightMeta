b624453710e29d365cbc248e9fab8c963761bbb2
Long On <on.long.on@goomail.fake>
2013-10-21 00:38:42 -0400

    Markup Group detail page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def get_user_name(user, project_maintainers)
     user.in_maintainers(project_maintainers) ? "*" + user.mu_name : user.mu_name
   end
+  def max_project_count(projects, limit=3)
+    count = projects.length > 0 ? projects.length : 0
+    count = count >= limit ? limit : count
+    count
+  end
 end
