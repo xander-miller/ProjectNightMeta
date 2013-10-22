ab022d58134c55a418e5b88aa04050726e857219
Xander Miller <xander.miller@gmail.com>
2013-10-22 18:57:27 -0400

    Front End: + Reactivated Rails-Sass gem + Added colors through scss + Refactored Home Jumbotron

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131019201055) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accesses", force: true do |t|
     t.integer  "user_id",                           null: false
     t.string   "provider",       default: "meetup"
